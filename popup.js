632eb94c4b5d70d6ad4c18c5d28e656bb942d377
bilkentCrap <bilkentCrap@users.noreply.github.com>
2017-04-02 22:10:35 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,19 @@
 function l() {
+	var e = (new Date()).getTime() / 1000;
+	var hashObj = new jsSHA("SHA-256", "TEXT", {numRounds: 1});
+	hashObj.update(e + "CMZ9rBFf40WNSSrsUdgQ");
 	chrome.storage.local.get(["id", "pass"], function(d) {
 		$("input[name=ID]").val(d["id"]);
 		$("input[name=PWD]").val(d["pass"]);
 		if ($("button").html() == "Go") {
 			$.ajax({
 				url: "https://stars.bilkent.edu.tr/srs/ajax/login.php",
-				data: { ID: d["id"], PWD: d["pass"] },
+				data: { A: "4045089c-a25f-11e6-80f5-76304dec7eb7",
+					B: hashObj.getHash("HEX"),
+					C: e,
+					ID: d["id"],
+					PWD: d["pass"]
+				},
 				method: "POST",
 				success: function(resp) {
 					if (resp == "HOME") {
